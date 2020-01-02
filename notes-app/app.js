a8ef4142a292bd2c59d353d769cebf11f3daee8d
Manish Hooda <mhooda2412@gmail.com>
2020-01-02 14:55:56 +0530

    first commint for notes aap using nodejs

---
+++ notes-app/app.js
@@ -0,0 +1,57 @@
+//importing required modules
+const Notes = require('./notes.js')
+const validator = require('validator')
+const yargs = require('yargs')
+
+//defining the yargs command to take input from command line
+//Add command for adding new notes
+yargs.command({
+	command:"add",
+	describe:"adding new notes",
+	builder :{
+		title:{
+			describe:"title of note",
+			demandOption:true,
+			type:'string'
+		},
+		body:{
+			describe:"body of note",
+			demandOption:true,
+			type:'string'
+		}
+	},
+	handler: (argv)=>Notes.addNote(argv.title,argv.body)	
+})
+
+// remove command to remove a note
+yargs.command({
+	command:"remove",
+	describe:"removing notes",
+	builder:{
+		title:{
+			describe:"title of node to be removed",
+			demandOption:true,
+			type:'string'
+		}
+	},
+	handler:(argv)=>Notes.removeNote(argv.title)
+})
+
+// list all the notes
+
+yargs.command({
+	command:"list",
+	describe:"listing all note",
+	handler:()=>console.log("list of notes")
+})
+
+// read the specified note 
+
+yargs.command({
+	command:"read",
+	describe:"read a note",
+	handler:()=>console.log("content of the  note")
+})
+
+//calling yargs 
+yargs.parse()
---
+++ notes-app/notes.js
@@ -0,0 +1,70 @@
+//importing file system module
+const fs = require("fs")
+const chalk = require("chalk")
+
+//getNotes function
+
+const getNotes =()=>{
+	return "your notes"
+}
+
+//adding new note
+
+const addNote =(title , body)=>{
+	const notes = lodeNotes()
+	const dublicateNotes = notes.filter((note)=> note.title === title)
+	if(dublicateNotes.length===0){
+		notes.push({
+			title: title,
+			body:body
+		})
+		saveNotes(notes)
+		console.log(chalk.green.inverse("Sucessfuly added note"));	
+	}
+	else {
+		console.log(chalk.yellow.inverse("title is already taken"));
+	}
+}
+
+//remove node fuction 
+const removeNote = (title)=>{
+	const notes = lodeNotes()
+	const notesToKeep = notes.filter((note)=>note.title !== title)
+	if(notesToKeep.length<notes.length){
+		saveNotes(notesToKeep)
+		console.log(chalk.green.inverse("Sucessfuly removed "));
+	}else{
+		console.log(chalk.red.inverse("note not found"));
+	}
+}
+
+//lode the alreade saved notes
+
+const lodeNotes = ()=>{
+	try{
+		const dataBuffer = fs.readFileSync("Notes.json")
+		const dataJSON = dataBuffer.toString()
+		return JSON.parse(dataJSON)
+
+	}catch(e){
+		return []
+
+	}
+
+}
+
+//saving the notes 
+
+const saveNotes = (notes)=>{
+	const dataJSON = JSON.stringify(notes);
+	fs.writeFileSync("Notes.json", dataJSON)
+}
+
+
+//exporting getnotes and addNote function
+
+module.exports = {
+	getNotes : getNotes,
+	addNote : addNote,
+	removeNote : removeNote
+}
